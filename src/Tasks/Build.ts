6c847af41d82721bdbd07d669f2d9f1eea56bddc
EccentricVamp <EccentricVamp@gmail.com>
2021-06-05 23:36:48 +0000

    Convert build to get energy from stores.

--- src/Tasks/Build.ts
+++ src/Tasks/Build.ts
@@ -1,40 +1,39 @@
 import Task from "Tasks/Task";
 export default class Build implements Task {
-  private source: Source;
+  private source: StructureContainer | StructureStorage;
   private target: ConstructionSite;
 
-  public constructor(source: Source, target: ConstructionSite) {
+  public constructor(source: StructureContainer | StructureStorage, target: ConstructionSite) {
     this.source = source;
     this.target = target;
   }
 
   public interview(creep: Creep): number | null {
     if (creep.getActiveBodyparts(CARRY) > 0) {
-      return creep.getActiveBodyparts(CARRY) + creep.getActiveBodyparts(WORK) +
-        creep.getActiveBodyparts(MOVE);
+      return creep.getActiveBodyparts(CARRY) + creep.getActiveBodyparts(WORK) + creep.getActiveBodyparts(MOVE);
     } else return null;
   }
 
   public perform(creep: Creep): boolean {
-    const HARVESTING = 2;
     const BUILDING = 1;
+    const WITHDRAW = 2;
 
     if (creep.memory.status === null || (creep.memory.status !== BUILDING && creep.store.getFreeCapacity() === 0)) {
       creep.memory.status = BUILDING;
       creep.say("🏗️ build");
     }
 
-    if (creep.memory.status !== HARVESTING && creep.store[RESOURCE_ENERGY] === 0) {
-      creep.memory.status = HARVESTING;
-      creep.say("⚒️ harvest");
+    if (creep.memory.status !== WITHDRAW && creep.store[RESOURCE_ENERGY] === 0) {
+      creep.memory.status = WITHDRAW;
+      creep.say("⇩ withdraw");
     }
 
     if (creep.memory.status === BUILDING) {
       if (creep.build(this.target) === ERR_NOT_IN_RANGE) {
         creep.moveTo(this.target, { visualizePathStyle: { stroke: "#ffffff" } });
       }
-    } else if (creep.memory.status === HARVESTING) {
-      if (creep.harvest(this.source) === ERR_NOT_IN_RANGE) {
+    } else if (creep.memory.status === WITHDRAW) {
+      if (creep.withdraw(this.source, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
         creep.moveTo(this.source, { visualizePathStyle: { stroke: "#ffaa00" } });
       }
     }
