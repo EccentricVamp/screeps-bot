202afae273aa9680c127b34bfa350df73f26a117
EccentricVamp <EccentricVamp@gmail.com>
2021-06-05 20:46:52 +0000

    Add renew logic.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Build from "Tasks/Build";
 import Harvest from "Tasks/Harvest";
+import Renew from "Tasks/Renew";
 import Task from "Tasks/Task";
 //import Upgrade from "Tasks/Upgrade";
 import _ from "lodash";
@@ -11,16 +12,28 @@ declare global {
 }
 
 export const loop = (): void => {
+  const RENEWING = 99;
+  const RENEW_THRESHOLD = 500;
+
+  const tasks = new Array<Task>();
   const room = Object.values(Game.rooms)[0];
   const spawn = room.find(FIND_MY_SPAWNS)[0];
   const creeps = room.find(FIND_MY_CREEPS);
-  const tasks = new Array<Task>();
   const sites = room.find(FIND_CONSTRUCTION_SITES);
+
+  const renew = new Renew(spawn);
+  for(const creep of creeps) {
+    if (creep.ticksToLive !== undefined && creep.ticksToLive < RENEW_THRESHOLD || creep.memory.status === RENEWING) {
+      const complete = renew.perform(creep);
+      if (complete) creep.memory.status = null;
+      else _.remove(creeps, creep);
+    }
+  }
 
   if (spawn.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
     const source = spawn.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
     if (source !== null) {
-      const harvest = new Harvest(1, RESOURCE_ENERGY, source, spawn);
+      const harvest = new Harvest(RESOURCE_ENERGY, source, spawn);
       tasks.push(harvest);
     }
   }
@@ -29,7 +42,7 @@ export const loop = (): void => {
     const site = sites[0];
     const source = site.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
     if (source !== null) {
-      const build = new Build(0.5, source, site);
+      const build = new Build(source, site);
       tasks.push(build);
     }
   }
