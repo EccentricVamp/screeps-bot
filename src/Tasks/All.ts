8fce2eca5585b284b419419c67de8bb986124475
EccentricVamp <EccentricVamp@gmail.com>
2021-06-06 17:13:51 +0000

    Add repair task.

--- src/Tasks/All.ts
+++ src/Tasks/All.ts
@@ -3,8 +3,9 @@ import Harvest from "./Harvest";
 import Idle from "./Idle";
 import Recycle from "./Recycle";
 import Renew from "./Renew";
+import Repair from "./Repair";
 import Task from "./Task";
 import Transport from "./Transport";
 import Upgrade from "./Upgrade";
 
-export { Build, Harvest, Idle, Recycle, Renew, Task, Transport, Upgrade };
+export { Build, Harvest, Idle, Recycle, Renew, Repair, Task, Transport, Upgrade };
---
+++ src/Tasks/Repair.ts
@@ -0,0 +1,52 @@
+import Task from "Tasks/Task";
+export default class Repair implements Task {
+  private source: StructureContainer | StructureStorage;
+  private target: Structure;
+
+  public constructor(source: StructureContainer | StructureStorage, target: Structure) {
+    this.source = source;
+    this.target = target;
+  }
+
+  public interview(creep: Creep): number | null {
+    const work = creep.getActiveBodyparts(WORK);
+    if (work === 0) return null;
+
+    const carry = creep.getActiveBodyparts(CARRY);
+    if (carry === 0) return null;
+
+    const move = creep.getActiveBodyparts(MOVE);
+    if (move === 0) return null;
+
+    return work + carry + move;
+  }
+
+  public perform(creep: Creep): boolean {
+    const REPAIRING = 1;
+    const WITHDRAW = 2;
+
+    if (creep.memory.status === null || (creep.memory.status !== REPAIRING && creep.store.getFreeCapacity() === 0)) {
+      creep.memory.status = REPAIRING;
+      creep.say("🛠️ repair");
+    }
+
+    if (creep.memory.status !== WITHDRAW && creep.store[RESOURCE_ENERGY] === 0) {
+      creep.memory.status = WITHDRAW;
+      creep.say("⚡ withdraw");
+    }
+
+    if (creep.memory.status === REPAIRING) {
+      if (creep.pos.inRangeTo(this.target.pos, 3)) {
+        creep.repair(this.target);
+      } else {
+        creep.moveTo(this.target, { visualizePathStyle: { stroke: "#ffffff" } });
+      }
+    } else if (creep.memory.status === WITHDRAW) {
+      if (creep.withdraw(this.source, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(this.source, { visualizePathStyle: { stroke: "#ffaa00" } });
+      }
+    }
+
+    return this.target.hits === this.target.hitsMax;
+  }
+}
