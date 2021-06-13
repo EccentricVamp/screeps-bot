5887b3fbde1be2bd7d6defe920962a2e014ba3b8
EccentricVamp <EccentricVamp@gmail.com>
2021-06-13 13:33:53 +0000

    Clean up rollup.config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,23 @@
 "use strict";
 
-import del from 'rollup-plugin-delete';
-import typescript from '@rollup/plugin-typescript';
-import screeps from 'rollup-plugin-screeps';
+import del from "rollup-plugin-delete";
+import screeps from "rollup-plugin-screeps";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
   input: "src/main.ts",
+  external: ["lodash"],
   output: {
     file: "dist/main.js",
     format: "cjs",
     interop: "esModule"
   },
-
-  external: ["lodash"],
-
   plugins: [
     del({ targets: "dist/*" }),
-    typescript({ tsconfig: "./tsconfig.json" }),
     screeps({
       configFile: "./screeps.json",
       dryRun: !process.env.publish ?? true
-    })
+    }),
+    typescript({ tsconfig: "./tsconfig.json" })
   ]
 }
