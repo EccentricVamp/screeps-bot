c3738e2098e4a66b2a2522dd803cddf6592c27d4
EccentricVamp <EccentricVamp@gmail.com>
2021-06-13 22:08:54 +0000

    Add Act interface.

--- src/Act/BaseAct.ts
+++ src/Act/BaseAct.ts
@@ -1,6 +1,15 @@
 export type MoveToReturnCode = CreepMoveReturnCode | ERR_NO_PATH | ERR_INVALID_TARGET | ERR_NOT_FOUND;
 
-export abstract class BaseAct<T extends { pos: RoomPosition }> {
+export interface Act<T> {
+  parts: Set<BodyPartConstant>;
+  resources: Set<ResourceConstant>;
+  target: T;
+
+  execute(creep: Creep): ScreepsReturnCode;
+  move(creep: Creep): MoveToReturnCode;
+}
+
+export abstract class BaseAct<T extends { pos: RoomPosition }> implements Act<T> {
   public parts: Set<BodyPartConstant>;
   public resources: Set<ResourceConstant>;
   public target: T;
