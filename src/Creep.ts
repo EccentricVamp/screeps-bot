e632fd0e0fcc676c6fc9273123d7ac7f7e667d8b
= <EccentricVamp@gmail.com>
2021-06-20 19:24:24 +0000

    Add default status to getStatus.

--- src/Creep.ts
+++ src/Creep.ts
@@ -8,9 +8,12 @@ export function getParts(creep: Creep): BodyPartConstant[] {
   return creep.body.map(part => part.type);
 }
 
-export function getStatus(creep: Creep): number | null {
-  if (creep.memory.status === undefined) creep.memory.status = null;
-  return creep.memory.status;
+export function getStatus(creep: Creep, defaultStatus: number | null = null): number | null {
+  const status = creep.memory.status;
+  if (status === undefined || status === null) {
+    creep.memory.status = defaultStatus;
+    return defaultStatus;
+  } else return status;
 }
 
 export function setStatus<T extends number | null>(creep: Creep, status: T): T {
