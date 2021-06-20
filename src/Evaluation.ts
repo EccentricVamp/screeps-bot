3086f6a6ff0337107fb39dfc9058226ea4e46744
= <EccentricVamp@gmail.com>
2021-06-20 18:51:50 +0000

    Store creep with evaluation.

--- src/Evaluation.ts
+++ src/Evaluation.ts
@@ -2,10 +2,13 @@ import { getParts } from "Creep";
 import _ from "lodash";
 
 export class Evaluation {
+  public creep: Creep;
   public eligible: boolean = true;
   public score: number = 0;
 
   constructor(creep: Creep, parts: BodyPartConstant[]) {
+    this.creep = creep;
+
     const partCounts = _.countBy(getParts(creep));
     for (const part in parts) {
       const count = partCounts[part];
