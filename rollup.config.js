813d9e71269e5e28e2ce40bfc75ef9df8901e34f
= <EccentricVamp@gmail.com>
2021-07-03 21:54:07 +0000

    Remove file extensions from output.

--- rollup.config.js
+++ rollup.config.js
@@ -4,20 +4,21 @@ import del from "rollup-plugin-delete";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: [
-    "src/main.ts",
-    "src/Constants.ts",
-    "src/Creep.ts",
-    "src/Filter.ts",
-    "src/Act/Act.ts",
-    "src/Task/Task.ts",
-  ],
+  input: {
+    main: "src/main.ts",
+    constants: "src/Constants.ts",
+    creep: "src/Creep.ts",
+    filter: "src/Filter.ts",
+    act: "src/Act/Act.ts",
+    task: "src/Task/Task.ts",
+  },
   external: ["lodash"],
   output: {
     dir: "dist",
     format: "cjs",
     interop: "esModule",
-    esModule: false
+    esModule: false,
+    entryFileNames: "[name]"
   },
   plugins: [
     del({ targets: "dist/*" }),
